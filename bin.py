6701adff2780445f619b2da8ddb12c5c90348402
isabelsilv <62412524+isabelsilv@users.noreply.github.com>
2022-03-03 22:15:52 +0100

    fin

--- bin.py
+++ bin.py
@@ -1,4 +1,3 @@
-
 n = int(input("type a numbers: "))
 m = int(input("type another number: "))
 
@@ -8,8 +7,11 @@ list = []
 list2 = []
 j = 0
 a = n*m
+
+##multiplication entre n et m
 print(a)
 
+##liste des nombres inférieurs à n et m
 while n>=z :
 	list.append(z)
 	list2.append(i)
@@ -19,22 +21,20 @@ while n>=z :
 
 list = list[::-1]
 list2 = list2[::-1]
-print(list)
-print(list2)
+
 list1=[]
+
+
+##opération permettant de trouver les nombres dont la somme est égale à n*m
 for num in list2:
-	print(a)
 	while num<=a:
 		a = a - num
 		list1.append(num)
 
-print(list1)
-count = 0
-for s in list1:
-	count += s
-print(count)		
+count = 0		
 
-print()
+s = " + ".join([str(l) for l in list1])
+print("l'addition ",s, "=", n*m)
 
 
 		
