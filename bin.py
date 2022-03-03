98cb658c07b8d3a4a89a96c1aeb831a7cf8681e7
isabelsilv <62412524+isabelsilv@users.noreply.github.com>
2022-03-03 21:47:52 +0100

    vai agora

--- bin.py
+++ bin.py
@@ -1,3 +1,4 @@
+
 n = int(input("type a numbers: "))
 m = int(input("type another number: "))
 
@@ -9,15 +10,39 @@ j = 0
 a = n*m
 print(a)
 
-while z<=n :
-	print("%(z)d| %(i)d" %{"z":z,"i":i})
-	z = z*2
-	i = m*z
+while n>=z :
 	list.append(z)
 	list2.append(i)
+	print("%(z)d| %(i)d" %{"z":z,"i":i})
+	z*=2
+	i = m*z
+
+list = list[::-1]
+list2 = list2[::-1]
 print(list)
 print(list2)
-  
+list1=[]
+for num in list2:
+	print(a)
+	while num<=a:
+		a = a - num
+		list1.append(num)
+
+print(list1)
+count = 0
+for s in list1:
+	count += s
+print(count)		
+
+print()
+
+
+		
+		
+	
+
+
+		
 	
 	
 		
